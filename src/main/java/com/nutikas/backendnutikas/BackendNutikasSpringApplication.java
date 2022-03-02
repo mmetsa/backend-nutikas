c6335b068da88c6e427554a048ae1e84dfc71bf4
mmetsa <mmetsa@ttu.ee>
2022-03-02 15:26:16 +0200

    Initial commit

---
+++ src/main/java/com/nutikas/backendnutikas/BackendNutikasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nutikas.backendnutikas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNutikasSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendNutikasSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nutikas/backendnutikas/BackendNutikasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutikas.backendnutikas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNutikasSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
