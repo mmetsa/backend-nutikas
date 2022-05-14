e5539f19aa43465eea3a48dbc96aaef43f6eee56
Maiko Metsalu <metsalumaiko@gmail.com>
2022-05-15 00:27:11 +0300

    Empty project commit

---
+++ src/main/java/ee/nutikas/games/api/BackendGamesApiApplication.java
@@ -0,0 +1,13 @@
+package ee.nutikas.games.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGamesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/nutikas/games/api/BackendGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.nutikas.games.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
